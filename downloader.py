835f7af854f43cf29cf553cc90a51bef46afd61f
MichaelM7337 <mmiroshnichenko2005@gmail.com>
2024-10-15 13:24:09 +0200

    Button markup added

--- downloader.py
+++ downloader.py
@@ -1,62 +1,84 @@
-import urllib
-import urllib.request
-from bs4 import BeautifulSoup
-from os import system
-import sys
+import os
+from pytube import YouTube
+from pydub import AudioSegment
+from youtubesearchpython import VideosSearch
 
 
-# def titleCase(s):
-#     '''
-#     A function to convert the given song name to title case.
-#     The default function in Python convert the words 'the', 'and', 'of', etc.
-#     And I find that annoying
-#     '''
-#
-#     l = s.split()
-#     str = l[0][0].upper() + l[0][1:]
-#
-#     for word in l[1:]:
-#         if word not in ['in', 'the', 'for', 'of', 'a', 'at', 'an', 'is', 'and']:
-#             str += ' ' + word[0].upper() + word[1:]
-#         else:
-#             str += ' ' + word
-#
-#     return str
+# Function to search YouTube for a song
+def search_youtube(song_name):
+    try:
+        # Perform the search on YouTube
+        videos_search = VideosSearch(song_name, limit=1)
+        result = videos_search.result()
+
+        if result and result['result']:
+            video_info = result['result'][0]
+            video_url = video_info['link']
+            print(f"Found YouTube video: {video_info['title']} ({video_url})")
+            return video_url
+        else:
+            print("No video found for the song name.")
+            return None
+    except Exception as e:
+        print(f"An error occurred while searching: {e}")
+        return None
 
 
-pathToSave = "~/Music/"
+# Function to download YouTube video
+def download_youtube_video(youtube_link):
+    try:
+        yt = YouTube(youtube_link)
+        # Download the highest quality audio stream
+        video = yt.streams.filter(only_audio=True).first()
+        out_file = video.download(output_path=".")
+
+        print(f"Downloaded video: {out_file}")
+        return out_file
+    except Exception as e:
+        print(f"An error occurred while downloading: {e}")
+        return None
 
 
-def getVidID(song, URL):
-    '''
-    This function gets the ID of the Video you have to download.
-    '''
-    search = song + ' lyrics'
-    searchQuery = '+'.join(search.split())
-    searchURL = URL + searchQuery
+# Function to convert video to MP3
+def convert_to_mp3(video_file):
+    try:
+        base, ext = os.path.splitext(video_file)
+        mp3_file = base + '.mp3'
 
-    response = urllib.request.urlopen(searchURL)
-    soup = BeautifulSoup(response.read(), "lxml")
+        # Convert to mp3 using pydub
+        audio = AudioSegment.from_file(video_file)
+        audio.export(mp3_file, format="mp3")
 
-    vidID = soup.body.find_all(class_="yt-uix-tile-link")[0]['href']
-    return vidID
+        # Optionally delete the original video file
+        os.remove(video_file)
 
-
-def downloadSong(song):
-    print("Downloading " + song)
-    URL = 'https://www.youtube.com/results?search_query='
-    vidID = getVidID(song, URL)
-    link = 'https://www.youtube.com' + vidID
-    system("youtube-dl -x -q -o \'" + pathToSave + song + ".%(ext)s\' \'" + link + "\'")
-    print("Downloaded " + song + "\n")
+        print(f"Converted to MP3: {mp3_file}")
+        return mp3_file
+    except Exception as e:
+        print(f"An error occurred during conversion: {e}")
+        return None
 
 
-# def main():
-#     print('-------------------------------------------------------------')
-#     if (len(sys.argv) == 3 and (sys.argv[1] == '-i' or sys.argv[1] == '-I')):
-#         for song in open(sys.argv[2]).readlines():
-#             doStuff(song)
-#
-#     else:
-#         for song in sys.argv[1:]:
-#             doStuff(song)
+# Main function to search, download, and convert
+def download_music_by_name(song_name):
+    # Search for the song on YouTube
+    youtube_link = search_youtube(song_name)
+
+    if youtube_link:
+        # Download the YouTube video
+        video_file = download_youtube_video(youtube_link)
+
+        if video_file:
+            # Convert to MP3
+            mp3_file = convert_to_mp3(video_file)
+
+            if mp3_file:
+                print(f"Music downloaded and saved as MP3: {mp3_file}")
+            else:
+                print("Failed to convert video to MP3.")
+        else:
+            print("Failed to download video.")
+    else:
+        print("No YouTube link found for the song.")
+
+download_youtube_video('')
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import telebot
 from telebot import types
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
-from downloader import downloadSong
+from downloader import download_youtube_video
 from video_search import search
 
 API_TOKEN = '6987658935:AAFd_tGI8-rAZA-Q5OCNN1ep6ramcTwnN_4'
@@ -22,36 +22,61 @@ def send_info(message):
     # markup.add(buttons[0], buttons[1])
     # bot.send_message(message.chat.id, text='List of commands', reply_markup=markup)
 
-@bot.message_handler(commands=['menu'])
-def send_menu(message):
-    buttons = []
-    buttons.append(InlineKeyboardButton("Btn1", callback_data="btn1"))
-    buttons.append(InlineKeyboardButton("Btn2", callback_data="btn2"))
-    markup = types.InlineKeyboardMarkup(row_width=2)
-    markup.add(buttons[0], buttons[1])
-    bot.send_message(message.chat.id, text='List of commands', reply_markup=markup)
+# @bot.message_handler(commands=['menu'])
+# def send_menu(message):
+    # buttons = []
+    # buttons.append(InlineKeyboardButton("Btn1", callback_data="btn1"))
+    # buttons.append(InlineKeyboardButton("Btn2", callback_data="btn2"))
+    #
+    # markup = types.InlineKeyboardMarkup(row_width=2)
+    # markup.add(buttons[0], buttons[1])
+    # bot.send_message(message.chat.id, text='List of commands', reply_markup=markup)
 
 
-@bot.callback_query_handler(func=lambda call: True)
-def callback_query(call):
-    if call.data == "cb_yes":
-        bot.answer_callback_query(call.id, "Answer is Yes")
-    elif call.data == "cb_no":
-        bot.answer_callback_query(call.id, "Answer is No")
+# @bot.callback_query_handler(func=lambda call: True)
+# def callback_query(call):
+#     if call.data == "cb_yes":
+#         bot.answer_callback_query(call.id, "Answer is Yes")
+#     elif call.data == "cb_no":
+#         bot.answer_callback_query(call.id, "Answer is No")
 
+buttons = []
+markup_1 = types.InlineKeyboardMarkup(row_width=3)
+markup_2 = types.InlineKeyboardMarkup(row_width=3)
+#links =[]
 @bot.message_handler(func=lambda message: True)
 def message_handler(message):
     #bot.copy_message(message.chat.id, message)
     videos = search(message.text)
-    buttons = []
-    markup = types.InlineKeyboardMarkup(row_width=1)
     n = 0
     while n <= videos[0].__len__()-1:
         title = videos[0][n]['title']
-        buttons.append(InlineKeyboardButton(title, callback_data="btn"+str(n)))
-        markup.add(buttons[n])
+        buttons.append(InlineKeyboardButton(title, callback_data="btn" + videos[0][n]['link']))
+        if n <= videos[0].__len__()/2 -1 :
+        #links.append(videos[0][n]['link'])
+            markup_1.add(buttons[n])
+        else:
+            markup_2.add(buttons[n])
         n+=1
-    bot.send_message(message.chat.id, text='List', reply_markup=markup)
+    markup_1.add(InlineKeyboardButton('<', callback_data="BtnBack"), InlineKeyboardButton('♥', callback_data="BtnLike"),
+               InlineKeyboardButton('>', callback_data="BtnForward"))
+    markup_2.add(InlineKeyboardButton('<', callback_data="BtnBack"), InlineKeyboardButton('♥', callback_data="BtnLike"),
+               InlineKeyboardButton('>', callback_data="BtnForward"))
+    bot.send_message(message.chat.id, text='Запрошенные песни 1/2', reply_markup=markup_1)
+
+@bot.callback_query_handler(func=lambda call:True)
+def callback_buttons(call):
+    if call.data == 'BtnForward':
+        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                              text="Запрошенные песни 2/2", reply_markup=markup_2)
+    elif call.data == 'BtnBack':
+        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                              text="Запрошенные песни 1/2", reply_markup=markup_1)
+    else:
+        for i in buttons:
+            if call.data == i.callback_data:
+                #bot.answer_callback_query(call.id, i.text)
+                bot.send_audio(chat_id=call.message.chat.id, audio=download_youtube_video((i.callback_data)[3:]))
 
 # Start the bot
 if __name__ == '__main__':
